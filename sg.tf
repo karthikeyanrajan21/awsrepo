343bc302617374c5f4d92d5d38e15f5c8b1bb2fc
karthikeyanrajan21 <karthisk217@gmail.com>
2024-08-28 12:36:05 +0530

    sg

--- sg.tf
+++ sg.tf
@@ -16,6 +16,15 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   to_port           = 22
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.knsgq.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+
 resource "aws_vpc_security_group_egress_rule" "allow_all" {
   security_group_id = aws_security_group.knsgq.id
   cidr_ipv4         = "0.0.0.0/0"
