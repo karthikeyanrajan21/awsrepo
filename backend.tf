427c5cb08c269b70031928b50eb4f1339f799453
karthikeyanrajan21 <149597338+karthikeyanrajan21@users.noreply.github.com>
2024-08-28 17:38:02 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "kn21"
-    key            = "terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
-    dynamodb_table = "kndb"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = "kn21"
+#    key            = "terraform.tfstate"
+#    region         = "us-east-2"
+#    encrypt        = true
+#    dynamodb_table = "kndb"
+#  }
+#}
